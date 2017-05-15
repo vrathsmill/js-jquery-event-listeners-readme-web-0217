1f1bf12ef163238e44ee9c2d5171fc593a72099c
vrathsmill <vlrathsmill@gmail.com>
2017-05-15 14:05:09 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -2,6 +2,35 @@
 
 $(document).ready(function(){
 
-// call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
+
+  function getIt(){
+    $("p").on("click", function(){
+      alert('Hey')
+    })
+  }
+
+  function frameIt(){
+    $("img").on("load", function(){
+      $('img').addClass("tasty")
+    })
+  }
+
+  function pressIt(){
+    $("form").on("keydown", function(key){
+      if(key.which==71){
+        alert("you have pressed g!")
+      }
+    })
+  }
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.")
+  })
+}
